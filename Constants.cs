897994668a953eb9683f34ce1859cc331e2fe71e
Voyse Lando <wojciech.surdyk@proton.me>
2025-03-24 20:53:29 +0100

    Added constants class

---
+++ Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miw_genetic_alg
+{
+    public static class Constants
+    {
+        public static int BitsPerEntity = 3;
+    }
+}
