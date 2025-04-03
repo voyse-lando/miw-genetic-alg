39a0cb9642541db596c37f572af140c7704b0cf8
Voyse Lando <wojciech.surdyk@proton.me>
2025-04-03 21:11:53 +0200

    Combined classes into a common one

---
+++ Common.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miw_genetic_alg
+{
+    public static class Common
+    {
+        private static Random random = new Random();
+
+        public static Dictionary<byte[], double> CreateDecodeTable(double min, double max, int bitsPerParameter)
+        {
+            Dictionary<byte[], double> ret = new Dictionary<byte[], double>(new ArrayEqualityComparer());
+
+            for (uint i = 0; i <= Math.Pow(2, bitsPerParameter) - 1; ++i)
+            {
+                byte[] key = new byte[bitsPerParameter];
+                for (var b = 0; b < bitsPerParameter; ++b)
+                {
+                    key[bitsPerParameter - b - 1] = (byte)((i & (int)Math.Pow(2, b)) >> b);
+                }
+
+                ret[key] = min + i / (Math.Pow(2, bitsPerParameter) - 1) * (max - min);
+            }
+            return ret;
+        }
+
+        public static byte[][] CreatePopulation(int size, int parameters, int bitsPerParameter)
+        {
+            byte[][] pop = new byte[size][];
+
+            for (int i = 0; i < size; ++i)
+            {
+                pop[i] = new byte[parameters * bitsPerParameter];
+                for (int k = 0; k < parameters * bitsPerParameter; ++k)
+                {
+                    pop[i][k] = (byte)random.Next(2);
+                }
+            }
+
+            return pop;
+        }
+
+        public static byte[] GetParameter(byte[] entity, int idx, int bitsPerParameter)
+        {
+            return entity.Take(new Range(idx * bitsPerParameter, (idx + 1) * bitsPerParameter)).ToArray();
+        }
+
+        public static byte[] TournamentSelect(byte[][] population, double[] fitness, int tournamentSize)
+        {
+            long[] tournament = [];
+            for (var i = 0; i < tournamentSize; ++i)
+            {
+                long index = random.Next(population.Length);
+                if (tournament.Contains(index)) --i;
+                tournament = tournament.Append(index).ToArray();
+            }
+
+            long best = tournament.MaxBy(fitness.GetValue);
+            return (byte[])population[best].Clone();
+        }
+
+        public static void MutateRandom(ref byte[] entity)
+        {
+            var mutationIndex = random.Next(Constants.bitsPerEntity);
+            entity[mutationIndex] = (byte)(entity[mutationIndex] ^ 1);
+        }
+
+        public static byte[] BestEntity(byte[][] population, double[] fitness)
+        {
+            int bestIdx = 0;
+            for (int i = 0; i < population.Length; ++i)
+            {
+                if (fitness[bestIdx] < fitness[i]) bestIdx = i;
+            }
+
+            return (byte[])population[bestIdx].Clone();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,21 +3,16 @@ namespace miw_genetic_alg
 
     internal static class Program
     {
+        static readonly Random random = new Random();
+
         [STAThread]
         static void Main()
         {
             //ApplicationConfiguration.Initialize();
             //Application.Run(new Form1());
 
-            var decodeTable = Parameters.CreateDecodeTable(-1.0, 1.0, 3);
-            string table = "";
-            for (uint i = 0; i <= (uint)Math.Pow(2, 3) - 1; i++)
-            {
-                byte[] key = [ (byte)((i & 4) >> 2), (byte)((i & 2) >> 1), (byte)(i & 1) ];
-                var value = decodeTable[key];
-                table += "" + key[0] + key[1] + key[2] + " => " + value + '\n';
-            }
-            MessageBox.Show(table);
+            Exercise1.GeneticAlgorithm();
         }
+
     }
 }
